8c05d91800efb9a8242ca56b4256c0493086f4fd
Philip Whitfield <public@underdoeg.com>
2011-07-16 19:29:54 +0200

    warn if constructor is called, will be fixed...

--- src/ofxJelloSpringBody.cpp
+++ src/ofxJelloSpringBody.cpp
@@ -2,7 +2,7 @@
 
 void ofxJelloSpringBody::createBody()
 {
-
+	ofLog(OF_LOG_ERROR, "ERROR: DO NOT CALL ofxJelloSpringBody() without arguments");
 }
 void ofxJelloSpringBody::createBody(ofPolyline shape, float massPerPoint, ofPoint position, float rotation, ofPoint scale, bool kinematic)
 {
