eac6473894134b69e6d6e21d9cf393e3f395aa0a
Philip Whitfield <public@underdoeg.com>
2011-07-16 19:26:27 +0200

    gitignore

--- src/ofxJelloSpringBody.cpp
+++ src/ofxJelloSpringBody.cpp
@@ -2,7 +2,7 @@
 
 void ofxJelloSpringBody::createBody()
 {
-	body=new SpringBody();
+
 }
 void ofxJelloSpringBody::createBody(ofPolyline shape, float massPerPoint, ofPoint position, float rotation, ofPoint scale, bool kinematic)
 {
